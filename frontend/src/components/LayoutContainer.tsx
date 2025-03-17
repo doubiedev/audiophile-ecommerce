84f5c3e91003a3d27456eaae19f6ddf7de9663a2
doubiedev <thedoubiedev@gmail.com>
2025-03-17 13:14:21 +1100

    install react-responsive, update navbar, add hamburger menu

--- frontend/src/components/LayoutContainer.tsx
+++ frontend/src/components/LayoutContainer.tsx
@@ -1,16 +1,16 @@
 import { ReactNode } from "react";
 
 interface LayoutContainerProps {
-  children: ReactNode;
-  className?: string;
+    children: ReactNode;
+    className?: string;
 }
 
-const LayoutContainer = ({ children, className }: LayoutContainerProps) => {
-  return (
-    <div className={`mx-auto w-full max-w-[327px] md:max-w-[689px] lg:max-w-[1110px] ${className}`}>
-      {children}
-    </div>
-  );
+const LayoutContainer = ({ children }: LayoutContainerProps) => {
+    return (
+        <div className={"mx-auto w-full max-w-[1190px] px-[24px] md:px-[40px]"}>
+            {children}
+        </div>
+    );
 };
 
 export default LayoutContainer;
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import { NavLink, useLocation } from "react-router";
+import { useMediaQuery } from "react-responsive";
 import LayoutContainer from "./LayoutContainer";
 import Logo from "../assets/shared/desktop/logo.svg";
 import Cart from "../assets/shared/desktop/icon-cart.svg";
+import HamburgerMenu from "../assets/shared/tablet/icon-hamburger.svg";
 
 const Navbar = () => {
     // TODO: show specific navbar depending on if we are on homepage or not
@@ -11,36 +13,56 @@ const Navbar = () => {
         <>
             {location.pathname === "/" ?
                 <nav className="bg-[#141414] w-full">
-                    <LayoutContainer>
-                        <div className="w-full h-[89px] md:h-[96px] flex items-center justify-between">
-                            <Logo />
-                            <div>
-                                <NavLink to="/" className="text-white hover:text-orange-dark">Home</NavLink>
-                                <NavLink to="/headphones" className="text-white hover:text-orange-dark">Headphones</NavLink>
-                                <NavLink to="/speakers" className="text-white hover:text-orange-dark">Speakers</NavLink>
-                                <NavLink to="/earphones" className="text-white hover:text-orange-dark">Earphones</NavLink>
-                            </div>
-                            <Cart />
-                        </div>
-                    </LayoutContainer>
+                    <NavLinks />
                 </nav>
                 :
                 <nav className="bg-black w-full">
-                    <LayoutContainer>
-                        <div className="w-full h-[89px] md:h-[96px] flex items-center justify-between">
-                            <Logo />
-                            <div>
-                                <NavLink to="/" className="text-white hover:text-orange-dark">Home</NavLink>
-                                <NavLink to="/headphones" className="text-white hover:text-orange-dark">Headphones</NavLink>
-                                <NavLink to="/speakers" className="text-white hover:text-orange-dark">Speakers</NavLink>
-                                <NavLink to="/earphones" className="text-white hover:text-orange-dark">Earphones</NavLink>
-                            </div>
-                            <Cart />
-                        </div>
-                    </LayoutContainer>
+                    <NavLinks />
                 </nav>}
         </>
-    )
+    );
+}
+
+const NavLinks = () => {
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
+    const isDesktop = useMediaQuery({ minWidth: 1024 });
+
+    return (
+        <LayoutContainer>
+            <div className="w-full h-[89px] md:h-[96px] flex items-center justify-between">
+                {isMobile ?
+                    <>
+                        <div className="flex items-center">
+                            <HamburgerMenu />
+                        </div>
+                        <Logo />
+                        <Cart />
+                    </> : <></>}
+                {isTablet ?
+                    <>
+                        <div className="flex gap-x-[42px]">
+                            <div className="flex items-center">
+                                <HamburgerMenu />
+                            </div>
+                            <Logo />
+                        </div>
+                        <Cart />
+                    </> : <></>}
+                {isDesktop ?
+                    <>
+                        <Logo />
+                        <div className="flex gap-x-[34px]">
+                            <NavLink to="/" className="text-white hover:text-orange-dark">Home</NavLink>
+                            <NavLink to="/headphones" className="text-white hover:text-orange-dark">Headphones</NavLink>
+                            <NavLink to="/speakers" className="text-white hover:text-orange-dark">Speakers</NavLink>
+                            <NavLink to="/earphones" className="text-white hover:text-orange-dark">Earphones</NavLink>
+                        </div>
+                        <Cart />
+                    </> : <></>}
+            </div>
+        </LayoutContainer>
+    );
 }
 
 export default Navbar;
