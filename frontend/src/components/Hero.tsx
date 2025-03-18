8a314d382aa34c84f9b4969358d1da7225109066
doubiedev <thedoubiedev@gmail.com>
2025-03-18 11:49:04 +1100

    add mobile and tablet hero images

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,14 +1,30 @@
-import imageHero from "../assets/home/desktop/image-hero.jpg";
+import { useResponsive } from "../contexts/ResponsiveContext";
+import imageHeroDesktop from "../assets/home/desktop/image-hero.jpg";
 import imageHeroTablet from "../assets/home/tablet/image-header.jpg";
 import imageHeroMobile from "../assets/home/mobile/image-header.jpg";
 
 const Hero = () => {
+    const { isMobile, isTablet, isDesktop } = useResponsive();
     return (
-        <div className="bg-[#191919] h-[510px] md:h-[639px]">
-            <img
-                src={imageHero}
-                className="w-screen h-[510px] md:h-[639px] object-contain"
-            />
+        <div className="bg-[#191919] h-[510px] md:h-[639px] flex justify-center">
+            {isDesktop && (
+                <img
+                    src={imageHeroDesktop}
+                    className="h-[510px] md:h-[639px] object-contain"
+                />
+            )}
+            {isTablet && (
+                <img
+                    src={imageHeroTablet}
+                    className="w-screen h-[510px] md:h-[639px] object-contain"
+                />
+            )}
+            {isMobile && (
+                <img
+                    src={imageHeroMobile}
+                    className="w-screen h-[510px] md:h-[639px] object-contain"
+                />
+            )}
         </div>
     );
 };
