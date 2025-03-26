67e8b29f85cf5dbafab6c97fb826bc189a00d997
doubiedev <thedoubiedev@gmail.com>
2025-03-26 12:18:48 +1100

    add category cards and category components

---
+++ frontend/src/components/Categories.tsx
@@ -0,0 +1,17 @@
+import headphones from "../assets/shared/desktop/image-category-thumbnail-headphones.png";
+import speakers from "../assets/shared/desktop/image-category-thumbnail-speakers.png";
+import earphones from "../assets/shared/desktop/image-category-thumbnail-earphones.png";
+import CategoryCard from "./CategoryCard";
+
+// TODO: Responsive sizes for Categories
+const Categories = () => {
+    return (
+        <div className="flex gap-[30px] h-[284px]">
+            <CategoryCard image={headphones} title="Headphones" />
+            <CategoryCard image={speakers} title="Speakers" />
+            <CategoryCard image={earphones} title="Earphones" />
+        </div>
+    );
+};
+
+export default Categories;
--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,33 +1,12 @@
 import LayoutContainer from "../components/LayoutContainer";
-import BtnShop from "../components/BtnShop";
-import headphones from "../assets/shared/desktop/image-category-thumbnail-headphones.png";
-import speakers from "../assets/shared/desktop/image-category-thumbnail-speakers.png";
-import earphones from "../assets/shared/desktop/image-category-thumbnail-earphones.png";
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
     return (
         <LayoutContainer>
-            <div className="mt-[120px] flex gap-[30px]">
-                <div className="relative h-[284px] w-full">
-                    <div className="z-[-1] absolute top-0 h-[80px] w-full bg-white"></div>
-                    <div className="z-[-1] absolute top-[80px] h-[204px] w-full bg-grey-dark"></div>
-                    <div className="h-full">
-                        <img src={headphones} className="object-scale-down" />
-                        <h6>Headphones</h6>
-                        <BtnShop />
-                    </div>
-                </div>
-                <div className="">
-                    <img src={speakers} />
-                    <h6>Speakers</h6>
-                    <BtnShop />
-                </div>
-                <div className="">
-                    <img src={earphones} />
-                    <h6>Earphones</h6>
-                    <BtnShop />
-                </div>
-            </div>
+            <section className="my-[6rem]">
+                <Categories />
+            </section>
             <section>Products Showcase Component</section>
             <section>About us Component</section>
         </LayoutContainer>
