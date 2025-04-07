09476aa9087f02d3757a8801a429794ed0e09948
doubiedev <thedoubiedev@gmail.com>
2025-04-07 12:52:36 +1000

    fix btn hover only on text

--- frontend/src/components/BtnTransparent.tsx
+++ frontend/src/components/BtnTransparent.tsx
@@ -1,9 +1,7 @@
 const BtnTransparent = () => {
     return (
-        <button className="w-[10rem] h-[3rem] bg-transparent border hover:bg-black">
-            <p className="subtitle text-black hover:text-white hover:cursor-pointer">
-                See Product
-            </p>
+        <button className="w-[10rem] h-[3rem] bg-transparent border hover:bg-black subtitle text-black hover:text-white hover:cursor-pointer">
+            See Product
         </button>
     );
 };
