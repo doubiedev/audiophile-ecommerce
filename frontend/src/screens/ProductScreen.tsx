149c8d7fbf02964c23740308944d64263acda1e3
doubiedev <thedoubiedev@gmail.com>
2025-04-14 12:10:56 +1000

    add features and in-the-box

--- frontend/src/screens/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen.tsx
@@ -27,22 +27,43 @@ const ProductScreen = () => {
 
     return (
         <LayoutContainer>
-            <section>
-                <p
-                    onClick={() => navigate(-1)}
-                    className="opacity-50 mt-[80px] mb-[3.5rem] hover:cursor-pointer"
-                >
-                    Go Back
-                </p>
+            <p
+                onClick={() => navigate(-1)}
+                className="opacity-50 mt-[80px] mb-[3.5rem] hover:cursor-pointer"
+            >
+                Go Back
+            </p>
+            <div className="flex flex-col gap-[10rem]">
                 <ProductCard id={productId} isPurchasable={true} />
-
-                <div>Features</div>
-                <div>In the box</div>
+                <div className="flex gap-[125px]">
+                    <div className="flex flex-col gap-[2rem] w-[635px]">
+                        <h3>Features</h3>
+                        <p className="opacity-50">
+                            {product.features[0]} <br /> <br />{" "}
+                            {product.features[1]}
+                        </p>
+                    </div>
+                    <div className="flex flex-col gap-[2rem] w-[350px]">
+                        <h3>In the box</h3>
+                        <div className="flex flex-col gap-[0.5rem]">
+                            {product.inTheBox.map((item, idx) => (
+                                <div className="flex" key={idx}>
+                                    <div className="w-[39px]">
+                                        <p className="text-orange-dark text-bold">
+                                            {item.quantity}x&nbsp;
+                                        </p>
+                                    </div>
+                                    <p className="opacity-50">{item.item}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
                 <div>Product Showcase Images</div>
                 <div>You may also like</div>
                 <Categories />
                 <BestGear />
-            </section>
+            </div>
         </LayoutContainer>
     );
 };
